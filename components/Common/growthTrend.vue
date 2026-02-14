b983002b9a63291310cf711fc5c7dc34128cb0cf
HimanshuYadav1811 <himanshuyadav181995@gmail.com>
2026-02-14 10:07:20 +0530

    update

--- components/Common/growthTrend.vue
+++ components/Common/growthTrend.vue
@@ -34,7 +34,7 @@
                 <div class="col-md-6">
                     <div class="image-box">
                         <div class="image-box-inner">
-                            <img src="~/assets/img/common/marketdata.svg" alt="" srcset="">
+                            <img src="~/assets/img/common/marketdata2.svg" alt="" srcset="">
                         </div>
                     </div>
                 </div>
