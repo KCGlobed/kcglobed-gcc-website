db0a6275bb0b36e37ca6f42863176e123b95f743
HimanshuYadav1811 <himanshuyadav181995@gmail.com>
2026-02-19 10:44:12 +0530

    nfet

--- pages/nfet.vue
+++ pages/nfet.vue
@@ -3,8 +3,7 @@
         <LayoutTopHeader />
         <LayoutMainNavbar />
         <!-- <CommonCommingSoon /> -->
-
-
+        <NfetBanner />
         <LayoutMainFooter />
         <LayoutCopyRight />
     </div>
