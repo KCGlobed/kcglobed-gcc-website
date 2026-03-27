4d98873598c3d5730ae784a337ad4678602e8bc0
suneel <suneel.kumar@kcglobed.com>
2026-03-27 18:13:13 +0530

    change the logo

--- pages/reset-password.vue
+++ pages/reset-password.vue
@@ -13,7 +13,7 @@
                 <!-- Brand logo inside card -->
                 <div class="card-logo">
                     <div class="logo-mark">
-                        <img src="/img/GCC-School-Logo-White.jpeg" alt="" />
+                        <img src="~/assets/Logo/Logo/GCC-School-Logo-White.jpeg" alt="" />
                     </div>
                 </div>
 
