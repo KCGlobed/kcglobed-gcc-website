9ea190860849c8c045aefe22c54fba7f1bf6f458
suneel <suneel.kumar@kcglobed.com>
2026-04-16 10:32:56 +0530

    working on news section

---
+++ pages/news.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <LayoutTopHeader class="style2-top-header" />
+    <LayoutMainNavbar />
+    <CommonInnerPageBanner pageTitle="News & Updates" mainTitle="NEWS" />
+    <NewsListing />
+    <LayoutMainFooter />
+    <LayoutCopyRight />
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: "News & Updates | GCC School",
+  meta: [
+    { name: 'description', content: 'Stay updated with the latest news, announcements, and insights from GCC School and KC GlobEd.' }
+  ]
+})
+</script>
+
+<style scoped>
+/* Any page-specific styles if needed */
+</style>
