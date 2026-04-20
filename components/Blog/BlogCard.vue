c060d74b1ae90e72b55f55c9ce95b4811478d2b1
suneel <suneel.kumar@kcglobed.com>
2026-04-20 11:10:49 +0530

    update

--- components/Blog/BlogCard.vue
+++ components/Blog/BlogCard.vue
@@ -16,7 +16,7 @@
       </h3>
       <div class="meta text-secondary fs-6">
         <NuxtLink :to="`/blogs/${blog.slug}`" class="text-dark text-decoration-none title-link">
-          <span class="fs-7">By </span><strong class="text-dark">KC GlobEd</strong> &mdash; {{ formattedDate }}
+          <span class="">By </span><strong class="text-dark">GCC School</strong> &mdash; {{ formattedDate }}
         </NuxtLink>
       </div>
     </div>
