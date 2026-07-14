138f692bf19354ee743d679035afc1ba0bdea58b
rupesh <102rupeshsharma@gmail.com>
2026-07-14 11:49:46 +0530

    Fixed hamburger issue in phone

--- components/Layout/NavbarStyleTwo.vue
+++ components/Layout/NavbarStyleTwo.vue
@@ -2,15 +2,16 @@
   <nav :class="['navbar navbar-expand-lg top-navbar', { sticky: isSticky }]">
     <div class="container-fluid">
       <NuxtLink class="navbar-brand" to="/">
-        <img src="~/assets/Logo/Logo/GCC-School-Logo-White.jpeg" class="navbar-logo" style="width: 140px;" alt="logo" loading="lazy" decoding="async" />
+        <img src="~/assets/Logo/Logo/GCC-School-Logo-White.jpeg" class="navbar-logo" style="width: 140px;" alt="logo"
+          loading="lazy" decoding="async" />
       </NuxtLink>
-      <a class="navbar-toggler home1-one-toggler" @click="stateStoreInstance.onMobileNavbarShow">
+      <button class="navbar-toggler home1-one-toggler" type="button" @click.prevent="stateStoreInstance.onMobileNavbarShow" @touchend.prevent="stateStoreInstance.onMobileNavbarShow" style="background: none; border: none; padding: 0; outline: none; box-shadow: none;">
         <span class="burger-menu">
           <span class="top-bar"></span>
           <span class="middle-bar"></span>
           <span class="bottom-bar"></span>
         </span>
-      </a>
+      </button>
       <div class="collapse navbar-collapse">
         <ul class="navbar-nav ms-auto">
           <!-- <li class="nav-item">
